046c0a89324a47d58d02853e4a079c8d0b6304d0
unknown <evandrosimendes@gmail.com>
2017-11-07 19:39:17 -0200

    First commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('goals',[
+      transition('* => *',[
+        query(':enter', style({ opacity: 0}), {optional : true}),
+        query(':enter', stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({ opacity: 0, transform :'translateY(-75%)', offset: 0}),
+            style({ opacity: .5, transform :'translateY(35px)', offset: .3}),
+            style({ opacity: 1, transform :'translateY(0)', offset: 1}),
+          ]))]), {optional : true}),
+        query(':leave', stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({ opacity: 1, transform :'translateY(0)', offset: 0}),
+            style({ opacity: .5, transform :'translateY(35px)', offset: .3}),
+            style({ opacity: 0, transform :'translateY(-75%)', offset: 1}),
+          ]))]), {optional : true})
+      ])
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+  
+  itemCount : number =4;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals = ['My first life goal','Add an Item', 'row teste'];
+
+  constructor() { }
+
+  ngOnInit() {
+    const self = this;
+    self.itemCount = self.goals.length;
+  }
+
+
+  addItem() {    
+    const self = this;
+    if(self.goalText != '' && self.goalText != 'Write an goal' ){
+      self.goals.push(self.goalText);
+      self.goalText = '';
+      self.itemCount = self.goals.length;
+    } else {
+      self.goalText = 'Write an goal';
+    }
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
+  }
+
+}
